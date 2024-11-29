527746330517fade20296b68f383813601ee87b8
isabellacpmelo <isacpmelo@gmail.com>
2024-11-28 22:20:08 -0300

    feat: Customizing background

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,14 @@ console.warn('Cupcake Store starting soon')
 </script>
 
 <template>
-  <div>Cupcake Store (Soon)</div>
+  <div>
+    <div class="bg-blue h-50 flex justify-center items-center gap-4">
+      <h1 class="text-[50px]">
+        Cupcake Store (Soon)
+      </h1>
+      <p>Icon Here</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
